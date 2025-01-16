480301812d74693cab7be4640a9f1fe06acaa093
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2025-01-16 18:09:54 +0200

    half finsh

--- scripts/main.js
+++ scripts/main.js
@@ -43,12 +43,12 @@ const shadowHeader = () => {
     ? header.classList.add("shadow-header")
     : header.classList.remove("shadow-header");
 };
-window.addEventListener('scroll', shadowHeader)
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== HOME SWIPER ===============*/
 let swiperHome = new Swiper(".home__swiper", {
   loop: true,
-  spaceBetween: 24, // بدل -24
+  spaceBetween: -24, // بدل -24
   grabCursor: true,
   slidesPerView: "auto",
   centeredSlides: true, // استخدم true بدلاً من 'auto'
@@ -58,7 +58,7 @@ let swiperHome = new Swiper(".home__swiper", {
   },
   breakpoints: {
     1220: {
-      spaceBetween: 32, // بدل -32
+      spaceBetween: -32, // بدل -32
     },
   },
 });
@@ -84,15 +84,112 @@ let swiperFeatured = new Swiper(".featured__swiper", {
   },
 });
 
-
 /*=============== NEW SWIPER ===============*/
 
+let swiperNew = new Swiper(".new__swiper", {
+  loop: true,
+  spaceBetween: 16, // بدل -24
+
+  slidesPerView: "auto",
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+    },
+  },
+});
+
 /*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBetween: 16, // بدل -24
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  window.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+  sections.forEach((current) => {
+    const sectionTop = current.offsetTop;
+    const sectionHeight = current.offsetHeight;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      ".nav__menu a[href*=" + sectionId + "]"
+    );
+
+    if (scrollDown >= sectionTop && scrollDown < sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// استرجاع الوضع المختار مسبقًا
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// الحصول على الوضع الحالي
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// التحقق من وجود إعدادات مسبقة وتطبيقها
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// إضافة حدث عند النقر لتبديل الوضع
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(".home__data , .featured__container , .new__container");
+sr.reveal(".home__images", { delay: 600 });
+sr.reveal(".services__card", { interval: 100 });
+sr.reveal(".discount__data", { origin: "left" });
+sr.reveal(".discount__images", { origin: "right" });
